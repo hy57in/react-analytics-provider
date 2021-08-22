b4ed8431a3ad2dc20e6b099a75468e43426283d4
naru200 <jinsu2504@gmail.com>
2021-08-22 12:14:30 +0900

    파일명을 exceptions에서 exception으로 수정

--- src/utils/googleAnalytics/index.ts
+++ src/utils/googleAnalytics/index.ts
@@ -2,7 +2,7 @@ import {event} from './event';
 import {click} from './click';
 import {initialize} from './initialize';
 import {set} from './set';
-import {exception} from './exceptions';
+import {exception} from './exception';
 
 export const googleAnalytics = {
   initialize,
