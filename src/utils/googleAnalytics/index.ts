9dd4cc8aa5c656699be3f027579adb70b7cc6f6e
naru200 <jinsu2504@gmail.com>
2021-08-22 11:55:32 +0900

    googleAnalytics/exceptions.ts 파일 추가

--- src/utils/googleAnalytics/index.ts
+++ src/utils/googleAnalytics/index.ts
@@ -2,10 +2,12 @@ import {event} from './event';
 import {click} from './click';
 import {initialize} from './initialize';
 import {set} from './set';
+import {exception} from './exceptions';
 
 export const googleAnalytics = {
   initialize,
   event,
+  exception,
   click,
   set,
 };
